d89ce05f5fe55f720fd7b4a87a0a71aff7bfb7fa
mitaraaa <rustam030201@gmail.com>
2022-09-03 17:39:16 +0600

    xd

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ def main():
     print(
         re.findall(r"\w+=([^,)]+)", str(psutil.sensors_temperatures()["battery"][0]))[1]
     )
+    battery = psutil.sensors_battery()
+    plugged = battery.power_plugged
+    percent = str(battery.percent)
+    plugged = "Plugged In" if plugged else "Not Plugged In"
+    print(percent + "% | " + plugged)
 
 
 main()
